1db7781dd72d79a0d3dbe2a9544ff678de1cf2e4
Bankzhy <bankzhy@gmail.com>
2024-10-05 21:19:23 +0900

    sample.java

---
+++ pretrain/sample.java
@@ -0,0 +1,19 @@
+  public int lengthOfLongestSubstring(String s) {
+    int start = 0;
+    int maxLen = 0;
+    HashMap<Character, Integer> map = new HashMap<>();
+
+    // Iterate through the string
+    for (int end = 0; end < s.length(); end++) {
+        char ch = s.charAt(end);
+          if (map.containsKey(ch) && map.get(ch) >= start) {
+
+
+              start = map.get(ch) + 1;
+          }
+          maxLen = Math.max(maxLen, end - start + 1);
+          map.put(ch, end);
+      }
+
+      return maxLen;
+  }
