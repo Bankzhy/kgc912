2e15ae412c13a34df82c367166ee5ffff8aff568
zhang.rog <bankzhy@gmail.com>
2024-12-11 12:05:59 +0900

    1120

--- pretrain/dataset.py
+++ pretrain/dataset.py
@@ -147,7 +147,7 @@ class KGCodeDataset(Dataset):
         nls = []
         docs = []
 
-        with open(file, encoding='utf-8') as f:
+        with open(file, encoding='UTF-8') as f:
             for line in f.readlines():
                 data = json.loads(line.strip())
                 code = data["code"]
