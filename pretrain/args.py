41110c6422f0ba12cc6d9a66b4cbec21050b631b
zhang.rog <bankzhy@gmail.com>
2025-01-03 21:11:47 +0900

    1120

--- pretrain/args.py
+++ pretrain/args.py
@@ -127,7 +127,7 @@ class RuntimeArguments:
     )
 
     n_epoch: int = field(
-        default=30,
+        default=10,
         metadata={'help': 'Number of data iterations for training'}
     )
 
