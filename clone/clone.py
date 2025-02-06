758df0721544db2cac7af068e3c00f40040a32db
zhang.rog <bankzhy@gmail.com>
2025-02-06 16:07:17 +0900

    1120

--- clone/clone.py
+++ clone/clone.py
@@ -48,7 +48,7 @@ def run_clone():
     # --------------------------------------------------
     # datasets
     # --------------------------------------------------
-    only_test = True
+    only_test = False
     logger.info('-' * 100)
     logger.info('Loading datasets')
     datasets = dict()
