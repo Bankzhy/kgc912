3bd343372a863afe6dd60834d982666720b18a07
zhang.rog <bankzhy@gmail.com>
2025-02-08 22:02:26 +0900

    1120

--- common/data_collator_kg.py
+++ common/data_collator_kg.py
@@ -106,7 +106,7 @@ def collate_fn(batch, args, task, code_vocab, nl_vocab, ast_vocab):
                                                      processor=Vocab.eos_processor,
                                                      max_len=args.max_nl_len)
 
-    elif task == "clone":
+    elif task == "clone2":
         code_1_raw, ast_1_raw, name_1_raw, code_2_raw, ast_2_raw, name_2_raw, labels = map(list, zip(*batch))
 
         model_inputs['input_ids'], model_inputs['attention_mask'] = get_concat_batch_inputs(
@@ -136,7 +136,7 @@ def collate_fn(batch, args, task, code_vocab, nl_vocab, ast_vocab):
             no_nl=args.no_nl
         )
         model_inputs['labels'] = torch.tensor(labels, dtype=torch.long)
-    elif task == "clone2":
+    elif task == "clone":
         code_1_raw, ast_1_raw, name_1_raw, code_2_raw, ast_2_raw, name_2_raw, labels = map(list, zip(*batch))
         code1_ids, code1_attention_mask = get_concat_batch_inputs(
             code_raw=code_1_raw,
