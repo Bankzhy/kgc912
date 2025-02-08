f6aeb5dc212a6a90a4bfc0f2ced95c5b7a87e026
zhang.rog <bankzhy@gmail.com>
2025-02-08 18:50:26 +0900

    1120

--- clone/args.py
+++ clone/args.py
@@ -21,7 +21,7 @@ class RuntimeArguments:
 
     trained_model: str = field(
         # default=None,
-        default='../pretrain/output/pretrain/models/cgp',
+        default='../pretrain/output5/pretrain/models/mnp',
         # default= r'C:\worksapce\research\kgc912\pretrain\output\pretrain\models\nlp',
         metadata={'help': 'Directory of trained model'}
     )
