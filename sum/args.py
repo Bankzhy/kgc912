3114bb8f0050ca287592a7df07d5c013e550daec
bankzhy <zhounan515130436@gmail.com>
2025-02-09 23:42:17 +0900

    20

--- sum/args.py
+++ sum/args.py
@@ -20,7 +20,7 @@ class RuntimeArguments:
     )
 
     trained_model: str = field(
-        default='../pretrain/output5/pretrain/models/mnp',
+        default='../pretrain/output/pretrain/models/mass',
         metadata={'help': 'Directory of trained model'}
     )
 
