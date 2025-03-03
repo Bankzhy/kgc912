cc1e739b720aa382454afe68637b2ddf13c2bf22
bankzhy <zhounan515130436@gmail.com>
2025-03-03 19:47:00 +0900

    20

--- pretrain/data_utils.py
+++ pretrain/data_utils.py
@@ -495,6 +495,8 @@ def tokenize_source(source, lang, use_regular=False):
         return trim_spaces(code)
     if lang == enums.LANG_PYTHON:
         tokens = tokenize.generate_tokens(StringIO(source).readline)
+        for token in tokens:
+            print(token)
         code = ' '.join([token.string for token in tokens])
         code = replace_string_literal(code)
         return trim_spaces(code)
