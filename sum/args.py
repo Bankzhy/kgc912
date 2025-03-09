ab19ee9035d4363e00fa2d382a6b0997618aad97
zhang.rog <bankzhy@gmail.com>
2025-03-09 12:04:33 +0800

    1120

--- sum/args.py
+++ sum/args.py
@@ -142,12 +142,12 @@ class RuntimeArguments:
     )
 
     batch_size: int = field(
-        default=64,
+        default=128,
         metadata={'help': 'Batch size for training on each device'}
     )
 
     eval_batch_size: int = field(
-        default=64,
+        default=128,
         metadata={'help': 'Batch size for evaluation on each device'}
     )
 
