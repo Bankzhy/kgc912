d155261d2d83f21a9d96f924e65f79f7a72cd1a6
zhang.rog <bankzhy@gmail.com>
2025-03-17 15:20:56 +0900

    1120

--- common/dataset.py
+++ common/dataset.py
@@ -219,7 +219,7 @@ class KGCodeDataset(Dataset):
             print("loading dataset:")
             for line in tqdm(lines):
                 # print(line)
-                try:
+                # try:
                     data = json.loads(line.strip())
                     doc = data["doc"]
                     st, nl = self.parse_kg(data["kg"])
@@ -249,9 +249,9 @@ class KGCodeDataset(Dataset):
                     structures.append(st)
                     nls.append(nl)
                     docs.append(doc)
-                except Exception as e:
-                    # print(e)
-                    continue
+                # except Exception as e:
+                #     print(e)
+                #     continue
 
         return codes, structures, nls, docs
 
