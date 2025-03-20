636235db39f50cbc572d068a471a4f2bd83e31bb
bankzhy <zhounan515130436@gmail.com>
2025-03-21 00:05:37 +0900

    20

--- clone/clone.py
+++ clone/clone.py
@@ -179,7 +179,7 @@ def run_clone():
         labels = eval_preds.label_ids
         gc.collect()
 
-        threshold = 0.7
+        threshold = 0.8
         # predictions = (logits >= threshold).astype(int).flatten()
         predictions = logits[:, 1] > threshold
         # predictions = np.argmax(logits, axis=-1)
